ad14c792c3d4c3027fc05e684a4df0b3bd9d1dc3
balamurugan-shanmugam <Balamurugan.Shanmugam@ucb.com>
2025-09-17 16:12:47 +0530

    Latest version
    Added Documentation part in the Function Scripts

--- R/rti.R
+++ R/rti.R
@@ -1,6 +1,33 @@
-# rti Function to create synthetic TI dataset
+# R/rti.R
+#' Create Random Trial Inclusion/Exclusion Criteria (TI) Dataset
+#'
+#' @description
+#' Creates a random SDTM TI (Trial Inclusion/Exclusion Criteria) dataset following CDISC SDTM standards.
+#' Trial Design. One record per I/E criterion, Tabulation.
+#'
+#' @param domain By default, value has been set as "TI", user can modify it if needed but not recommended
+#' @param ietestcd Values to be mapped under IETESTCD variable
+#' @param ietest Values to be mapped under IETEST variable
+#' @param iecat Values to be mapped under IECAT variable
+#' @param iescat Values to be mapped under IESCAT variable (optional)
+#' @param tirl Values to be mapped under TIRL variable (optional) - Expected format: IETESTCD|TIRL text. Each value must be separated by a pipe (|). IETESTCD values should match respective input.
+#' @param tivers Values to be mapped under TIVERS variable (optional) -  Instead of duplicating values like: tivers = c("1.0", "1.0", "1.0", "1.0", "1.0", "2.0", "2.0", "2.0", "2.0", "2.0"), can use the following format as well: tivers = c(rep("1.0", 5), rep("2.0", 5)).
+#' @param drop_vars List the Permissible variables with no values that needs to be dropped (optional) - Variabe names should be listed in UPPERCASE
+#'
+#' @return A data.frame with SDTM TI structure
+#' @export
+#'
+#' @examples
+#' rti(ietestcd = c("IN01", "IN02", "EX01", "EX02", "EX03", "IN01_V2", "IN02_V2", "EX01_V2", "EX02_V2", "EX03_V2"),
+#'     ietest = c("Inclusion Criteria 1", "Inclusion Criteria 2", "Exclusion Criteria 1", "Exclusion Criteria 2", "Exclusion Criteria 3", "Inclusion Criteria 1 V2", "Inclusion Criteria 2 V2", "Exclusion Criteria 1 V2", "Exclusion Criteria 2 V2", "Exclusion Criteria 3 V2"),
+#'     iecat = c("INCLUSION", "INCLUSION", "EXCLUSION", "EXCLUSION", "EXCLUSION", "INCLUSION", "INCLUSION", "EXCLUSION", "EXCLUSION", "EXCLUSION"),
+#'     iescat = c("Minor", rep(NA, 9)),
+#'     tirl = c("IN01|Inclusion 1 Rue", "EX02_V2|Rule for Exclusion 2"),
+#'     tivers = c(rep("1.0", 5), rep("2.0", 5)),
+#'     drop_vars = c())
+#'
+
 rti <- function(domain = "TI",
-                studyid = "AD2025",
                 ietestcd = c(),
                 ietest = c(),
                 iecat = c(),
@@ -8,7 +35,7 @@ rti <- function(domain = "TI",
                 tirl = c(),
                 tivers = c(),
                 drop_vars = c()){
- 
+
   # Metadata for the TI dataset
   ti_metadata <- list("STUDYID" = "Study Identifier",
                       "DOMAIN" = "Domain Abbreviation",
@@ -19,40 +46,40 @@ rti <- function(domain = "TI",
                       "TIRL" = "Inclusion/Exclusion Criterion Rule",
                       "TIVERS" = "Protocol Criteria Versions"
                       )
-  
+
   # Logic checks
   # Checking if all the required inputs have been provided
   if (missing(ietestcd) | missing(ietest) | missing(iecat)) {
     stop("Error: One or more Req variable input has NOT been provided. Make sure to add inputs for all the following parameters: ietestcd, ietest, iecat.")
   }
-  
+
   # No. of IETESTCD/IETEST provided should be matched
   if (length(ietestcd) != length(ietest)) {
     stop("Error: No. of IETESTCD and IETEST provided not matched")
   }
-  
+
   # No. of IETESTCD/IECAT provided should be matched
   if (length(ietestcd) != length(iecat)) {
     stop("Error: No. of IETESTCD and IECAT provided not matched")
   }
-  
+
   # If IESCAT parameter included, then No. of IETESTCD/IESCAT provided should be matched
   if (length(iescat) > 0 & length(iescat) != length(ietestcd)) {
     stop("Error: No. of IETESTCD and IESCAT provided not matched. Add NA values if any of the IETESTCD doesn't require IESCAT.")
   }
-  
+
   # Creation of dummy value for IESCAT, if IESCAT parameter is not included
   if (length(iescat) == 0) {
     iescat <- c(rep(NA, length(ietestcd)))
   }
-  
+
   # Checking if necessary inputs are provided for TIRL
   if (!is.null(tirl) & length(tirl) > 0) {
-    
-    tirl_df <- as.data.frame(tirl) %>% 
-               mutate(IETESTCD = sapply(strsplit(tirl, "\\|"), "[", 1),
-               TIRL = sapply(strsplit(tirl, "\\|"), "[", 2))
-    
+
+    tirl_df <- as.data.frame(tirl) %>%
+               mutate(IETESTCD = sapply(strsplit(as.character(tirl), "\\|"), "[", 1),
+               TIRL = sapply(strsplit(as.character(tirl), "\\|"), "[", 2))
+
     if (!(all(tirl_df$IETESTCD %in% ietestcd))){
       stop("Error: Invalid input for TIRL. Expected format: IETESTCD|TIRL text. Both values must be separated by a pipe (|). Check if the values match the provided IETESTCD input.")
     }
@@ -60,50 +87,39 @@ rti <- function(domain = "TI",
   } else {
     tirl_df <- data.frame(IETESTCD = character(), TIRL = character())
   }
-  
+
   # If TIVERS parameter included, then No. of IETESTCD/TIVERS provided should be matched
   if (length(tivers) > 0 & length(tivers) != length(ietestcd)) {
     stop("Error: No. of IETESTCD and TIVERS provided not matched. Add NA values if any of the IETESTCD doesn't require TIVERS.")
   }
-  
+
   # Creation of dummy value for TIVERS, if TIVERS parameter is not included
   if (length(tivers) == 0) {
     tivers <- c(rep(NA, length(ietestcd)))
   }
-  
+
   # Combining all the variables as a dataframe
   ti_vars <- data.frame(ietestcd, ietest, iecat, iescat, tivers)
-  
+
   # Naming all the variables in dataframe as in Uppercase
   names(ti_vars) <- toupper(names(ti_vars))
-  
+
   # Joining TIRL variable; Mapping STUDYID and DOMAIN
-  df1 <- ti_vars %>% 
-         left_join(tirl_df %>% select(IETESTCD, TIRL), by = c("IETESTCD" = "IETESTCD")) %>% 
+  df1 <- ti_vars %>%
+         left_join(tirl_df %>% select(IETESTCD, TIRL), by = c("IETESTCD" = "IETESTCD")) %>%
          mutate(STUDYID = studyid,
                 DOMAIN = domain) %>%
-         arrange(STUDYID, TIVERS, desc(IECAT), IETESTCD) %>% 
+         arrange(STUDYID, TIVERS, desc(IECAT), IETESTCD) %>%
          select(STUDYID, DOMAIN, IETESTCD, IETEST, IECAT, IESCAT, TIRL, TIVERS)
-  
+
   # Adding labels to the variables
   df2 <- apply_metadata(df1, ti_metadata)
-  
+
   # Drop Variables
   if (length(drop_vars) > 0) {
     df2 <- df2 %>% select(-all_of(drop_vars))
   }
-  
+
   # Final TI dataset
   assign("ti", df2, envir = .GlobalEnv)
 }
-
-# Sample calling of rti function
-rti(ietestcd = c("IN01", "IN02", "EX01", "EX02", "EX03", "IN01_V2", "IN02_V2", "EX01_V2", "EX02_V2", "EX03_V2"), 
-    ietest = c("Inclusion Criteria 1", "Inclusion Criteria 2", "Exclusion Criteria 1", "Exclusion Criteria 2", "Exclusion Criteria 3", 
-               "Inclusion Criteria 1 V2", "Inclusion Criteria 2 V2", "Exclusion Criteria 1 V2", "Exclusion Criteria 2 V2", "Exclusion Criteria 3 V2"),
-    iecat = c("INCLUSION", "INCLUSION", "EXCLUSION", "EXCLUSION", "EXCLUSION", "INCLUSION", "INCLUSION", "EXCLUSION", "EXCLUSION", "EXCLUSION"),
-    iescat = c("Minor", rep(NA, 9)),
-    tirl = c("IN01|Inclusion 1 Rue", "EX02_V2|Rule for Exclusion 2"),
-    tivers = c(rep("1.0", 5), rep("2.0", 5)),
-    drop_vars = c()
-    )
--- R/rtv.R
+++ R/rtv.R
@@ -1,6 +1,35 @@
-# rtv Function to create synthetic TV dataset
+# R/rtv.R
+#' Create Random Trial Visits (TV) Dataset
+#'
+#' @description
+#' Creates a random SDTM TV (Trial Visits) dataset following CDISC SDTM standards.
+#' Trial Design. One record per planned Visit per Arm, Tabulation.
+#'
+#' @param domain By default, value has been set as "TV", user can modify it if needed but not recommended
+#' @param visitnum Numeric vector - Values to be mapped under VISITNUM variable
+#' @param visit Values to be mapped under VISIT variable
+#' @param visitdy Numeric vector - Values to be mapped under VISITDY variable (optional)
+#' @param armcd Values to be mapped under ARMCD variable (optional)
+#' @param arm Values to be mapped under ARM variable (optional)
+#' @param tvstrl Values to be mapped under TVSTRL variable
+#' @param tvenrl Values to be mapped under TVENRL variable (optional)
+#' @param drop_vars List the Permissible variables with no values that needs to be dropped (optional) - Variabe names should be listed in UPPERCASE
+#'
+#' @return A data.frame with SDTM TV structure
+#' @export
+#'
+#' @examples
+#' rtv(visitnum = c(1, 2, 3, 4, 5, 6, 7, 8),
+#'     visit = c("Screening", "Baseline", "Week 2", "Week 4", "Week 6", "Week 8", "Week 10", "Follow-up"),
+#'     visitdy = c(-14, 1, 15, 29, 43, 57, 71, 99),
+#'     armcd = c(),
+#'     arm = c(),
+#'     tvstrl = c("Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose"),
+#'     tvenrl = c(),
+#'     drop_vars = c())
+#'
+
 rtv <- function(domain ="TV",
-                studyid = "AD2025",
                 visitnum = c(),
                 visit = c(),
                 visitdy = c(),
@@ -9,7 +38,7 @@ rtv <- function(domain ="TV",
                 tvstrl = c(),
                 tvenrl = c(),
                 drop_vars = c()) {
-  
+
   # Metadata for the TV dataset
   tv_metadata <- list("STUDYID" = "Study Identifier",
                       "DOMAIN" = "Domain Abbreviation",
@@ -20,98 +49,87 @@ rtv <- function(domain ="TV",
                       "ARM" = "Description of Planned Arm",
                       "TVSTRL" = "Visit Start Rule",
                       "TVENRL" = "Visit End Rule")
-  
+
   # Logic checks
   # Checking if all the required inputs have been provided
   if (missing(visitnum) | missing(visit) | missing(tvstrl)) {
     stop("Error: One or more Req variable input has NOT been provided. Make sure to add inputs for all the following parameters: visitnum, visit, tvstrl.")
   }
-  
+
   # Check if input for VISITNUM is provided with numeric values
   if (!is.numeric(visitnum)) {
     stop("Error: VISITNUM should be provided wirh numeric values")
   }
-  
+
   # Check if input for VISITDY is provided with numeric values
   if (length(visitdy) > 0 & !is.numeric(visitdy)) {
     stop("Error: VISITDY should be provided wirh numeric values")
   }
-  
+
   # No. of Visit/Visitnum provided should be matched
   if (length(visitnum) != length(visit)) {
     stop("Error: No. of VISITNUM and VISIT provided not matched")
   }
-  
+
   # No. of VISIT and TVSTRL provided should be matched
   if (length(visit) != length(tvstrl)) {
     stop("Error: No. of VISIT and TVSTRL provided not matched")
   }
-  
+
   # No. of VISIT and VISITDY provided should be matched
   if (length(visitdy) > 0 & length(visit) != length(visitdy)) {
     stop("Error: No. of VISIT and VISITDY provided not matched. Add NA values if any of the VISIT doesn't require VISITDY.")
   }
-  
+
   # Creation of dummy value for VISITDY, if VISITDY parameters are not included
   if (length(visitdy) == 0) {
     visitdy <- c(rep(NA, length(visit)))
   }
-  
+
   # No. of VISIT/ARM/ARNCD provided should be matched
   if ((length(arm) > 0 & length(visit) != length(arm)) | (length(armcd) > 0 & length(visit) != length(armcd))) {
     stop("Error: No. of VISIT and ARM/ARMCD values provided not matched")
   }
-  
+
   # Creation of dummy value for ARM/ARMCD, if ARM/ARMCD parameters are not included
   if (length(arm) == 0) {
     arm <- c(rep(NA, length(visit)))
   }
-  
+
   if (length(armcd) == 0) {
     armcd <- c(rep(NA, length(visit)))
   }
-  
+
   # If TVENRL is included No. of VISIT and TVENRL should be matched
   if (length(tvenrl) > 0 & length(tvenrl) != length(visit)) {
     stop("Error: No. of VISIT and TVENRL provided not matched. Add NA values if any of the VISIT doesn't require TVENRL.")
   }
-  
+
   # Creation of dummy value for TVENRL, if TVENRL parameter is not included
   if (length(tvenrl) == 0) {
     tvenrl <- c(rep(NA, length(visit)))
   }
-  
+
   # Combining all the variables as a dataframe
   visit_vars <- data.frame(visitnum, visit, visitdy, armcd, arm, tvstrl, tvenrl)
-  
+
   # Naming all the variables in dataframe as in Uppercase
   names(visit_vars) <- toupper(names(visit_vars))
-  
-  df1 <- visit_vars %>% 
+
+  df1 <- visit_vars %>%
          mutate(STUDYID = studyid,
                 DOMAIN = domain) %>%
-         arrange(ARMCD, VISITNUM, VISIT) %>% 
+         arrange(ARMCD, VISITNUM, VISIT) %>%
          select(STUDYID, DOMAIN, VISITNUM, VISIT, VISITDY, ARMCD, ARM, TVSTRL, TVENRL)
-  
+
   # Adding labels to the variables
   df2 <- apply_metadata(df1, tv_metadata)
-  
+
   # Drop Variables
   if (length(drop_vars) > 0) {
     df2 <- df2 %>% select(-all_of(drop_vars))
   }
-  
+
   # Final TV dataset
   assign("tv", df2, envir = .GlobalEnv)
 }
-
-# Sample calling of rtV function
-rtv(visitnum = c(1, 2, 3, 4, 5, 6, 7, 8),
-    visit = c("Screening", "Baseline", "Week 2", "Week 4", "Week 6", "Week 8", "Week 10", "Follow-up"),
-    visitdy = c(-14, 1, 15, 29, 43, 57, 71, 99),
-    armcd = c(),
-    arm = c(),
-    tvstrl = c("Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose", "Based on scheduled calendar days from first dose"),
-    tvenrl = c(),
-    drop_vars = c()
-    )
